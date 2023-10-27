3fb598250f643aec5fce2ac6decf5deb4b1e905b
Leonardo Muniz <leonardonm15@gmail.com>
2023-10-27 20:37:42 -0300

    b do div 3

--- codeforces/round_891/b.cpp
+++ codeforces/round_891/b.cpp
@@ -5,6 +5,31 @@ using namespace std;
 const int maxn = 1e5 + 5;
 
 void solve () {
+    string s; cin >> s;
+    int n = s.size();
+    int cum = 0;
+    int mj = maxn;
+    string c;
+    for (int i = n - 1; i >= 0; i--) {
+        if (s[i] - '0' >= 5) {
+            if (i == 0) {
+                cout << 1;
+                for (int j=0; j < n; j++) cout << 0;
+                cout << endl;
+                return;
+            }
+            s[i - 1]++;
+            s[i] = '0';
+            mj = i;
+        }
+        c.push_back(s[i]);
+    }
+    reverse(c.begin(), c.end());
+    for (int i=0; i < n; i++) {
+        if (i < mj) cout << c[i];
+        else cout << 0;
+    }
+    cout << endl;
 }
 
 signed main() {
