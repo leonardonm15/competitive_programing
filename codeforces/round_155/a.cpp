f8e61c470fbea1b72b7b52007bd53ffcb21f287a
Leonardo <leonardonm15@gmail.com>
2023-10-26 23:50:37 -0300

    nao sei pq essa a nao passa, serio mas passei a b anyway

---
+++ codeforces/round_155/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define int long long
+#define endl "\n"
+ 
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, a, b; cin >> n >> a >> b;
+        int x, y;
+        for (int i=1; i < n; i++) {
+            cin >> x >> y;
+            if (x >= a && y >= b) {
+                cout << -1 << endl;
+                return;
+            }
+        }
+        cout << a << endl;
+    }
+}
--- codeforces/round_155/b.cpp
+++ codeforces/round_155/b.cpp
@@ -6,20 +6,25 @@ const int maxa = 1e9;
 void solve () {
     int n; cin >> n;
     int mn1 = 1e9;
-    int min2 = 1e9;
+    int mn2 = 1e9;
     int resp1 = 0;
     int resp2 = 0;
     for (int i=0; i < n; i++) {
         int num; cin >> num;
         resp1 += num;
-        mn1 = min(mn, num);
+        mn1 = min(mn1, num);
     }
     for (int i=0; i < n; i++) {
         int num; cin >> num;
         resp2 += num;
-        mn2 = min(mn, num);
+        mn2 = min(mn2, num);
     }
-    if (
+    int k1 = mn1 * n;
+    int k2 = mn2 * n;
+    resp1 += k2;
+    resp2 += k1;
+    int resp = min(resp1, resp2);
+    cout << resp << endl;
 }
 
 signed main() {
