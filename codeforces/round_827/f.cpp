b6e8ba08a013dc5ba7e3d88fba78696a74227554
Leonardo Muniz <leonardonm15@gmail.com>
2023-11-28 19:08:20 -0300

    fzinha

--- codeforces/round_827/f.cpp
+++ codeforces/round_827/f.cpp
@@ -10,11 +10,42 @@ using namespace std;
 // ----------    GRIND MENTALITY    ---------     
 
 void solve () {
-
-    // recebe a string a e sorta ela crescente
-    // rebe a string b e sorta ela decrescente
-
-    // faz o valor base 26 das strings e multiplica por k
+    int n; cin >> n;
+    // After each operation, determine if it is possible to rearrange the characters of s and t 
+    // s < t
+    map<char, int> ms;
+    map<char, int> mt;
+    ms['a']++;
+    mt['a']++;
+    int sl = 1;
+    int tl = 1;
+    while (n--) {
+        int op, t; cin >> op >> t;
+        string s; cin >> s;
+        for (auto cara: s) {
+            if (op == 1) {
+                ms[cara] += t;
+                sl += t;
+            } else {
+                mt[cara] += t;
+                tl += t;
+            }
+        }
+        cout << "letras s -> " << sl << endl;
+        cout << "letras t -> " << tl << endl;
+        for (auto [letra, f]: ms) {
+            // aaab s 
+            // aabb
+            if (mt[letra] < f) {
+                cout << "YES" << endl;
+                break;
+            } else if (mt[letra] > f) {
+                cout << "NO" << endl;
+                break;
+            } 
+        }
+        if (ms == mt) cout << "NO" << endl;
+    }
 }
 
 signed main() {
