d2a66919932248cb92baca9f838e56ef22dc6f99
Leonardo Muniz <leonardonm15@gmail.com>
2023-12-26 19:25:33 -0300

    mais uma questãozinha

--- codeforces/round_902/c.cpp
+++ codeforces/round_902/c.cpp
@@ -2,79 +2,38 @@
 
 using namespace std;
 //#define int long long
-//
-int calc(int n, int mid) {
-    set<int> s;
-    /* cout << "calc -> "; */
-    for (int i=n; i >= 0; i--) {
-        s.emplace(mid);
-        /* cout << mid << " - "; */
-        mid %= i;
-        if (mid == 0) {
-            s.emplace(0);
-            break;
-        }
-    }
-    return s.size();
-}
 
 void solve () {
-    int n, r, k; cin >> n >> r >> k;
-    int r2 = r;
-    int l = 0;
-    int mid = (l + r) / 2;
-    int resp = -1;
-    
-    while (l <= r) {
-        mid = (l + r) / 2;
-        int x = calc(n, mid);
-        if (k <= x) {
-            resp = x;
-            r--;
-        } else {
-            l++;
-        }
-    }
-
-    /* cout << "resp -> " << resp << endl; */
-    if (r2 == 0 && k == 1) {
-        cout << 1 << endl;
-        return;
-    }
-    if (resp == -1) {
+    int n, m, k; cin >> n >> m >> k;
+    if (k > 3) {
         cout << 0 << endl;
         return;
     }
 
-    r = r2;
-    l = 0;
-    int answ = -1;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        int x = calc(n, mid);
-        /* cout << "mid -> " << mid << endl; */
-        /* cout << "calc -> " << calc(n, mid) << endl; */
-        if ((k + 1) <= x) {
-            answ = mid;
-            r--;
-        } else {
-            l++;
-        }
-    }
-    
-    for (int i=0; i <= r2; i++) {
-        cout << "calc - " << i << " -> " << calc(n, i) << endl;
+    if (k == 1) {
+        cout << 1 << endl;
+        return;
     }
 
-    /* cout << "answ -> " << answ << endl; */
-    /* cout << "r2 -> " << r2 << endl; */
-    /* if (answ == -1) { */
-    /*     cout << (r2 + 1) - resp << endl; */
-    /*     return; */
-    /* } else { */
-    /*     cout << (answ + 1) - resp << endl; */
-    /*     return; */
-    /* } */
+    if (k == 2 && m <= n) {
+        cout << m << endl;
+        return;
+    }
+
+    if (k == 2 && m > n) {
+        cout << n + ((m - n) / n) << endl;
+        return;
+    }
+
+    if (k == 3 && m <= n) {
+        cout << 0 << endl;
+        return;
+    }
+
+    if (k == 3 && m > n) {
+        cout << (m - n) - ((m - n) / n) << endl;
+        return;
+    }
 }
 
 signed main() {
@@ -82,7 +41,9 @@ signed main() {
     int TC = 1;
     if (TC){
         cin >> TC;
-        while (TC--) solve();
+        while (TC--) {
+            solve();
+        }
     } else {
         solve();
     }
