c0d64ac746db883228f7ff094ac42f95a338bfe8
Leonardo Muniz <leonardonm15@gmail.com>
2023-12-27 15:52:38 -0300

    passei vambora rumo rio

--- codeforces/round_640/e.cpp
+++ codeforces/round_640/e.cpp
@@ -5,38 +5,31 @@ using namespace std;
 
 void solve () {
     int n; cin >> n;
-    map<int, int> map;
     vector<int> pref(n + 1);
+    vector<int> freq(n + 1);
     vector<int> arr;
-    set<int> set;
+
     int soma = 0;
-    cout << "pref -> ";
     for (int i=0; i < n; i++) {
         int num; cin >> num;
+        freq[num]++;
         arr.push_back(num);
-        set.emplace(num);
         soma += num;
         pref[i + 1] = soma;
-        cout << pref[i + 1] << " ";
     }
-    cout << endl;
-    
+
     int p = 0;
     for (int i=1; i <= n; i++) {
         for (int j=2; (i - j) >= 0; j++) {
-        /* cout << "i - j -> " << i << " - " << j << endl; */
             int x = pref[i];
             int m = pref[(i - j)];
-            /* cout << "_---------------" << endl; */
-            /* cout << "map[x - m] -> " << x - m << endl; */
-            map[x - m]++;
+            if (x - m <= n && freq[x - m]) {
+                p += freq[x - m];
+                freq[x - m] = 0;
+            }
+            
         }
-        /* if (map[arr[i - 1]]) p++; */
     }
-    for (auto cara: set) {
-        if (map[cara]) p += map[cara];
-    }
-
     cout << p << endl;
 }
 
