9ebbda89a616b9531976faef6a85cf6adad97266
leonardonm15 <leonardonm15@gmail.com>
2024-01-12 22:58:06 -0300

    tava errada a minha ideia, mas consegui entender o editorial pelo menos

--- codeforces/round_863/d.cpp
+++ codeforces/round_863/d.cpp
@@ -3,18 +3,13 @@
 using namespace std;
 
 #define endl '\n' 
-#define int long long
+//#define int long long
 
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
 
 vector<int> fib(46);
-
-bool aob (int h, int w, int a, int b) {
-    if ((a > h || a <= 0) || (b > w || b <= 0)) return true;
-    else return false;
-}
 
 void build() {
     fib[0] = 1;
@@ -24,87 +19,25 @@ void build() {
     }
 }
 
+bool inside(int n, int x, int y) {
+    if (n == 1) return true; // chegou na coordenada final
+    // maior que o quadradão e menor que o retangulo
+    if (fib[n - 1] <= y && y < fib[n]) return false; 
+    if (fib[n] <= y) y -= fib[n]; // caso esteja na outra metade
+    return inside(n - 1, y, x); // fecha o quadrado verticalmente e horizontalmente
+}
+
 void solve () {
-    int n, y, x; cin >> n >> y >> x;
-    int h, w;
-    h = fib[n];
-    w = fib[n + 1];
-    /* cout << "fib -> "; */
-    /* for (int i=0; i <= 10; i++) cout << fib[i] << " "; */
-    /* cout << endl; */
-    cout << "h -> " << h << endl;
-    cout << "w -> " << w << endl;
-    // direita cima esquerda baixo
-    vector<pair<int, int>> mv = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-    for (int k=0; k < 4; k++) {
-        int q = k;
-        int a = x;
-        int b = y;
-        bool flag = true;
-        cout << " ---------------- " << endl;
-        for (int i=1; i <= n; i++) {
-            int op = q % 4;
-            cout << "op -> " << op << endl;
-            if (i > 1) {
-                a += (mv[op].first * fib[i]) + (mv[(op + 1) % 4].first * (fib[i] - 1));
-                b += (mv[op].second * fib[i]) + (mv[(op + 1) % 4].second * (fib[i] - 1));
-                op++;
-            } else {
-                /* cout << "(mv[op].first * fib[i]) -> " << (mv[op].first * fib[i]) << endl; */
-                /* cout << "(mv[op].second * fib[i]) -> " << (mv[op].second * fib[i]) << endl; */
-                a += (mv[op].first * fib[i]);
-                b += (mv[op].second * fib[i]);
-            }
-            cout << a << " - " << b << endl;
-            if (aob(h, w, b, a)) {
-                flag = false;
-                break;
-            }
-            q++;
-        }
-        if (flag) {
-            cout << "YES" << endl;
-            return;
-        }
-    }
-    cout << " ---------- INVERSO -----------" << endl;
-    for (int k=4; k > 0; k--) {
-        int q = k;
-        int a = x;
-        int b = y;
-        bool flag = true;
-        cout << " ---------------- " << endl;
-        for (int i=1; i <= n; i++) {
-            int op = q % 4;
-            cout << "op -> " << op << endl;
-            a += (mv[op].first * fib[i]) + (mv[(op - 1) % 4].first * (fib[i] - 1));
-            b += (mv[op].second * fib[i]) + (mv[(op - 1) % 4].second * (fib[i] - 1));
-            op--;
-            cout << a << " - " << b << endl;
-            if (aob(h, w, b, a)) {
-                flag = false;
-                break;
-            }
-            q--;
-        }
-        if (flag) {
-            cout << "YES" << endl;
-            return;
-        }
-    }
-
-
-
-    cout << "NO" << endl;
-
+    int n, x, y; cin >> n >> x >> y;
+    x--, y--;
+    cout << (inside(n, x, y) ? "YES" : "NO") << endl;
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
     int TC = 1;
+    build();
     if (TC){
-        build();
         cin >> TC;
         while (TC--) solve();
     } else {
