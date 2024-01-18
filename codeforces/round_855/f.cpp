b240b599359034429cf453008f41b3d77b0c38e2
leonardonm15 <leonardonm15@gmail.com>
2024-01-17 21:03:53 -0300

    passei

--- codeforces/round_855/f.cpp
+++ codeforces/round_855/f.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define endl '\n' 
 #define int long long
- 
+
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
 //
- 
+
 int cmp = 0b000000000011111111111111111111111111;
- 
+
 void solve () {
     int n; cin >> n;
-    map<pair<int, int>, int> f;
-    map<int, pair<int, int>> map;
+    map<int, int> miss[26][2];
     int resp = 0;
- 
+
     for (int i=0; i < n; i++) {
         string s; cin >> s;
-        int sz = s.size();
         int l = 0;
         int p = 0;
         for (auto c: s) {
@@ -28,30 +26,26 @@ void solve () {
             l |= (1 << k);
             p ^= (1 << k);
         }
- 
+        int sz = s.size() & 1;
+
         for (int i=0; i < 26; i++) {
-            int need = p ^ cmp;
-            if (((1 << i) & need)) {
-                need ^= (1 << i);
-                if ((map[need].first && (map[need].second + sz)&1)) {
-                    int k = map[need].first | l;
-                    int v = 0;
-                    for (int i=0; i < 26; i++) {
-                        if ((1 << i) & k) continue;
-                        else v++;
-                    }
-                    if (v == 1) {
-                        resp += f[{need, map[need].first}];
-                    }
-                }
+            int nao_tenho = l ^ cmp;
+            int need_p = p ^ cmp;
+            if ((1 << i) & nao_tenho) {
+                need_p &= ~(1 << i);
+                auto it = miss[i][!sz].find(need_p);
+                if (it != miss[i][!sz].end()) resp += it->second;
             }
         }
-        map[p] = {l, sz};
-        f[{p, l}]++;
+
+        int nao_tenho = l ^ cmp;
+        for (int i=0; i < 26; i++) {
+            if (nao_tenho & (1 << i)) miss[i][sz][p]++;
+        }
     }
     cout << resp << endl;
 }
- 
+
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
     int TC = 0;
