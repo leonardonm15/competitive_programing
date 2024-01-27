7b212c3ea6e39947a5cf5c5b485162a701a9b91d
leonardonm15 <leonardonm15@gmail.com>
2024-01-27 16:02:37 -0300

    a do codeforces

--- codeforces/round_899/b.cpp
+++ codeforces/round_899/b.cpp
@@ -1,33 +1,36 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 /* #define endl '\n' */ 
 #define int long long
- 
+
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
- 
+
 void solve () {
-    int a, b;
-    map<int, int> map;
-    for (int i=2; i <= 26; i++) {
-        cout << "? " << 3 << " " << i << endl;
-        cin >> a;
-        cout << "? " << i << " " << 3 << endl;
-        cin >> b;
-        if (a == -1) {
-            cout << "! " << i - 1 << endl;
-        } else if (a != b) {
-            cout << "! " << a + b << endl;
+    int n; cin >> n;
+    vector<vector<int>> arr(n);
+    for (int i=0; i < n; i++) {
+        int m; cin >> m;
+        vector<int> a;
+        for (int j=0; j < m; j++) {
+            int num; cin >> num;
+            a.push_back(num);
+        }
+        arr[i] = a;
+    }
+
+    for (int i=0; i < n; i++) {
+        for (int j=i; j < n; j++) {
         }
     }
 }
- 
+
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int TC = 0;
+    int TC = 1;
     if (TC){
         cin >> TC;
         while (TC--) solve();
