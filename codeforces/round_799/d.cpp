1481c0166d85a33b8e1c875140f88a559d38340e
leonardonm15 <leonardonm15@gmail.com>
2024-01-31 20:05:51 -0300

    correção do de hoje

--- codeforces/round_799/d.cpp
+++ codeforces/round_799/d.cpp
@@ -12,22 +12,22 @@ using namespace std;
 void solve () {
     string s; cin >> s;
     int k; cin >> k;
-    map<pair<int, int>, int> map;
+    map<int, int> map;
     int resp = 0;
     int h, m; 
 
     h = (s[0] - '0') * 10 + (s[1] - '0');
     m = (s[3] - '0') * 10 + (s[4] - '0');
 
-    int v = h * 60 + m;
-    while (true) {
-        int ch = (v / 60) % 24;
+    int v = (h * 60) + m;
+    while (!map[v]) {
+        int ch = (v / 60);
         int cm = v % 60;
-        cout << ch << ":" << cm << endl;
-        if (ch % 10 == cm / 10 && ch / 10 == cm % 10 && !map[{ch, cm}]) resp++;
-        if (cm == m && ch == h) break;
+        /* cout << ch << ":" << cm << endl; */
+        if ((ch % 10 == cm / 10) && (ch / 10 == cm % 10) && !map[v]) resp++;
+        map[v]++;
         v += k;
-        v %= 24 * 60;
+        v %= (24 * 60);
     }
 
     cout << resp << endl;
