6a7f1065795ee629566009bc86fadb1b68410351
leonardonm15 <leonardonm15@gmail.com>
2024-02-11 20:31:42 -0300

    passei a F, doideira

--- codeforces/round_923/f.cpp
+++ codeforces/round_923/f.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-#define endl '\n' 
+//#define endl '\n' 
 //#define int long long
 
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
 
-const int maxn = 1e5 + 5;
+const int maxn = 2e5 + 5;
 
 vector<vector<tuple<int, int, int>>> adj(maxn); // w v i
 vector<tuple<int, int, int, int>> edg;
@@ -26,9 +26,9 @@ void tarjan(int u, int p) {
     low[u] = timer;
     t[u] = timer;
     for (auto [w, v, i]: adj[u]) {
-        if (v == u) continue;
+        if (v == p) continue;
         if (vis[v]) {
-            low[u] = min(low[u], t[v]); // ve quando que eu entrei na backedge
+            low[u] = min(low[u], low[v]); // ve quando que eu entrei na backedge
         } else {
             tarjan(v, u);
             low[u] = min(low[v], low[u]);
@@ -39,19 +39,28 @@ void tarjan(int u, int p) {
     }
 }
 
+bool flag = false;
+vector<int> stc;
 void dfs(int u, int v, int k) {
+    /* cout << "u -> " << u << endl; */
+    stc.push_back(u);
     vis[u]++;
-    resp.push_back(u);
-    if (u == k) return;
+    if (u == k) {
+        for (auto cara: stc) resp.push_back(cara);
+        return;
+    }
     for (auto [w, c, i]: adj[u]) {
-        if (c != v && !bridge[i] && !vis[c]) {
+        if (c != v && !vis[c]) {
             dfs(c, u, k);
         }
     }
+    stc.pop_back();
 }
 
+int tc = 0;
 void solve () {
     int n, m; cin >> n >> m;
+    tc++;
     // recebe os caras
     for (int i=1; i <= m; i++) {
         int u, v, w; cin >> u >> v >> w;
@@ -60,6 +69,13 @@ void solve () {
         edg.push_back({w, u, v, i});
     }
 
+    /* if (tc == 22) { */
+    /*     cout << n << "#" << m << "#"; */
+    /*     for (auto [w, u, v, i]: edg) { */
+    /*         cout << u << "-" << v << "-" << w << "#"; */
+    /*     } */
+    /* } */
+
     // faz dfs, acha os ciclos e marca no is_bridge
     for (int i=1; i <= n; i++) {
         if (!vis[i]) tarjan(i, -1);
@@ -67,11 +83,25 @@ void solve () {
 
     sort(edg.begin(), edg.end());
 
+    /* cout << "edg -> "; */
+    /* for (auto [w, u, v, i]: edg) { */
+    /*     cout << w << " "; */
+    /* } */
+    /* cout << endl; */
+
+    /* cout << "bridge -> "; */
+    /* for (int i=0; i <= m; i++) cout << bridge[i] << " "; */
+    /* cout << endl; */
+
     for (int i=1; i <= n; i++) vis[i] = 0;
 
     for (auto [w, u, v, i]: edg) {
+        /* cout << "u -> " << u << endl; */
+        /* cout << "v -> " << v << endl; */
         /* cout << "w -> " << w << endl; */
+        /* cout << "-----------" << endl; */
         if (!bridge[i]) {
+            /* resp.push_back(v); */
             dfs(u, v, v);
             cout << w << " " << resp.size() << endl;
             for (auto cara: resp) cout << cara << " ";
@@ -82,11 +112,13 @@ void solve () {
 
     resp.clear();
     edg.clear();
+    stc.clear();
     for (int i=0; i <= m; i++) {
         vis[i] = 0;
         bridge[i] = false;
         adj[i].clear();
         low[i] = -1;
+        flag = false;
         t[i] = 0;
     }
 
