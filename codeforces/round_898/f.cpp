d84042f9aff824ce1d870d499bb1422e5a95d71e
leonardonm15 <leonardonm15@gmail.com>
2024-02-15 14:49:18 -0300

    joao fez a boa

--- codeforces/round_898/f.cpp
+++ codeforces/round_898/f.cpp
@@ -13,61 +13,58 @@ void solve () {
     int n, k; cin >> n >> k;
     vector<int> v(n);
     vector<int> h(n);
-    
-    for (int i=0; i < n; i++) cin >> v[i];
-    for (int i=0; i < n; i++) cin >> h[i];
-    
-    vector<int> arr;
+    vector<int> valid(n + 1);
+    vector<vector<int>> arr;
+
     int resp = 0;
     for (int i=0; i < n; i++) {
-        if (arr.size() == 0 || arr[arr.size() - 1] % h[i] == 0) arr.push_back(v[i]);
-        else {
-            int sum = 0;
-            vector<int> p;
-            p.push_back(0);
+        cin >> v[i];
+        if (v[i] <= k) resp = 1;
+    }
+    for (int i=0; i < n; i++) cin >> h[i];
+    h.push_back(1);
 
-            cout << "arr -> ";
-            for (auto cara: arr) cout << cara << " ";
-            cout << endl;
-
-            for (int j=0; j < arr.size(); j++) {
-                sum += v[j];
-                p.push_back(sum);
-            }
-
-            for (int j=0; j < arr.size(); j++) {
-                int idx = lower_bound(p.begin(), p.end(), arr[j] + k) - p.begin();
-                resp = max(resp, idx - j + 1);
-            }
-
-            arr.clear();
-            arr.push_back(v[i]);
-        }
-        
+    for (int i=0; i < n; i++) {
+        if (h[i] % h[i + 1] == 0) valid[i] = 1;
     }
 
-    if (arr.size() > 0) {
-        int sum = 0;
-        vector<int> p;
-        p.push_back(0);
-
-        cout << "arr -> ";
-        for (auto cara: arr) cout << cara << " ";
-        cout << endl;
-
-        for (int j=0; j < arr.size(); j++) {
-            sum += v[j];
-            p.push_back(sum);
+    vector<int> f;
+    f.push_back(0);
+    int sum = 0;
+    for (int i=0; i <= n; i++) {
+        if (valid[i]) {
+            sum += v[i];
+            f.push_back(sum);
+        } else {
+            if (i > 0 && i < n && valid[i - 1]) f.push_back(sum + v[i]);
+            arr.push_back(f);
+            sum = 0;
+            f.clear();
+            f.push_back(0);
         }
+    }
 
-        for (int j=0; j < arr.size(); j++) {
-            int idx = lower_bound(p.begin(), p.end(), arr[j] + k) - p.begin();
-            resp = max(resp, idx - j + 1);
+    for (auto vec: arr) {
+        for (int i=0; i < vec.size(); i++) {
+            int l = i;
+            int r = vec.size() - 1;
+            int t = 0;
+
+            while (l <= r) {
+                int mid = (l + r) >> 1;
+                if (vec[mid] - vec[i] > k) {
+                    r = mid - 1;
+                } else {
+                    t = mid - i;
+                    l = mid + 1;
+                }
+            }
+
+            resp = max(resp, t);
         }
     }
 
     cout << resp << endl;
-    
 }
 
 signed main() {
