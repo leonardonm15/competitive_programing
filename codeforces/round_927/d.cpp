9f7e4172431372c1fe6df112872a5871a8499c26
leonardonm15 <leonardonm15@gmail.com>
2024-02-21 17:19:39 -0300

    dznha

--- codeforces/round_927/d.cpp
+++ codeforces/round_927/d.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 #define endl '\n' 
 //#define int long long
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#ifndef ONLINE_JUDGE
+#define debarr(arr, n) cerr << "arr: ["; forn(i, n) cerr << arr[i] << (i == n-1 ? "" : ", "); cerr << "]" << endl;
+#else
+#define debarr(x...)
+#endif
+
 
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
@@ -16,56 +47,78 @@ void solve () {
 
     for (int i=0; i < n * 2; i++){
         string s; cin >> s;
-        map[s[0]].emplace(s[1] - '0');
+        map[s[1]].emplace(s[0] - '0');
     }
+
 
     vector<char> s = {'C', 'D', 'H', 'S'};
     for (int i=0; i < s.size(); i++) {
+        /* debug(map[s[i]]); */
         if (s[i] == c) {
             s.erase(s.begin() + i);
             break;
         }
     }
 
+
     vector<string> p;
     int k = 0;
     for (int i=0; i < n; i++) {
-        if (map[s[k]].size() == 0) k++;
+        /* debug(p); */
+        while (k < s.size() && map[s[k]].size() == 0) k++;
         
-        if (k > s.size()) {
+        /* debug(k); */
+        if (k >= s.size()) {
             while (map[c].size() > 0) {
                 if (map[c].size() == 1 || (*map[c].begin() == *map[c].rbegin())) {
                     cout << "IMPOSSIBLE" << endl;
                     return;
                 }
-
                 string u;
-                u += c + ('0' + *map[c].begin());
+
+                u.push_back(('0' + *map[c].begin()));
+                u.push_back(c);
                 p.push_back(u);
+
                 u.clear();
-                u += c + ('0' + *map[c].rbegin());
+
+                u.push_back(('0' + *map[c].rbegin()));
+                u.push_back(c);
                 p.push_back(u);
+
                 map[c].erase(map[c].begin());
-                map[c].erase(map[c].end()--);
+                map[c].erase(--map[c].end());
             }
+            break;
         }
 
         // pegar begin e rbegin
         if (map[s[k]].size() > 1 && (*map[s[k]].begin() != *map[s[k]].rbegin())) {
             string u;
-            u += s[k] + ('0' + *map[s[k]].begin());
+
+            u.push_back(('0' + *map[s[k]].begin()));
+            u.push_back(s[k]);
             p.push_back(u);
+
             u.clear();
-            u += s[k] + ('0' + *map[s[k]].rbegin());
+
+            u.push_back(('0' + *map[s[k]].rbegin()));
+            u.push_back(s[k]);
             p.push_back(u);
-            map[s[k]].erase(map[s[k]].end()--);
+
             map[s[k]].erase(map[s[k]].begin());
+            map[s[k]].erase(--map[s[k]].end());
         } else if (map[c].size() > 0) {
             // pegar o begin + um dos coringas repetidos
             // se nao tiver repetido pega o primeiro, se nao tiver retorna
             int lc = -1;
             int a = 0;
             for (auto cara: map[c]) {
+                if (a == 0) {
+                    a++;
+                    lc = cara;
+                    continue;
+                }
                 if (cara == lc) {
                     lc = cara;
                     break;
@@ -75,18 +128,19 @@ void solve () {
             }
             
             string u;
-            u += s[k] + ('0' + *map[s[k]].begin());
+            u.push_back(('0' + *map[s[k]].begin()));
+            u.push_back(s[k]);
             p.push_back(u);
             u.clear();
 
             if (lc == -1) {
-                u += c + ('0' + *map[c].begin());
+                u.push_back(('0' + *map[c].begin()));
+                u.push_back(c);
                 map[c].erase(map[c].begin());
             } else {
-                u += c + ('0' + lc);
-                auto idx = map[c].begin();
-                advance(idx, a);
-                map[c].erase(idx);
+                u.push_back(('0' + lc));
+                u.push_back(c);
+                map[c].erase(--map[c].end());
             }
 
             p.push_back(u);
@@ -99,9 +153,9 @@ void solve () {
 
     }
 
-    cout << p[0] << " " << p[1] << endl;
-
-    
+    for (int i=0; i < 2 * n; i += 2) {
+        cout << p[i] << " " << p[i + 1] << endl;
+    }
 }
 
 signed main() {
