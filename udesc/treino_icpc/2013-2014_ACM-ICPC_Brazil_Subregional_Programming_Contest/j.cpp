6cf995f91d9ddd87597e9b1d6a625e0550c4ef83
leonardonm15 <leonardonm15@gmail.com>
2024-03-05 00:07:24 -0300

    aprendendo um pouco mais de binary lifting e sparse talbe

--- udesc/treino_icpc/2013-2014_ACM-ICPC_Brazil_Subregional_Programming_Contest/j.cpp
+++ udesc/treino_icpc/2013-2014_ACM-ICPC_Brazil_Subregional_Programming_Contest/j.cpp
@@ -15,24 +15,57 @@ const int N = 2e4 + 5;
 const int M = 1e5 + 5; 
 vector<pii> adj[N];
 
+vector<int> sz(N);
+vector<int> daddy(N);
+vector<int> vis(N);
+vector<vector<int>> st(N, vector<int>(log(N)));
+
+int c = 0;
+int find(int a) {
+    return (daddy[a] == a ? a : daddy[a] = find(daddy[a]));
+}
+
+void dfs(int u, int p, int d) {
+    for (auto cara: adj[u]) {
+        for (int j = 0; j * 2 <= d; j *= 2) {
+
+        }
+    }
+}
+
 void solve () {
     int n, m, q; cin >> n >> m >> q;
     vector<tuple<int, int, int>> edg;
+    for (int i=0; i <= n; i++) {
+        daddy[i] = i;
+        st[i][0] = 0;
+        sz[i] = 1;
+    }
+
     for (int i=0; i < m; i++) {
         int a, b, w; cin >> a >> b >> w;
         edg.push_back({w, a, b});
     }
 
     sort(edg.rbegin(), edg.rend());
-    /* cout << "--------" << endl; */
+    cout << "--------" << endl;
     for (auto [w, a, b]: edg) {
         /* cout << w << " "; */
-        if (adj[a].size() == 0 || adj[b].size() == 0) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) {
             /* cout << a << " " << b << " " << w << endl; */
+            if (sz[pb] > sz[pa]) swap(pa, pb);
+            daddy[pb] = daddy[pa];
+            sz[a] += sz[b];
             adj[a].push_back({b, w});
             adj[b].push_back({a, w});
         }
     }
+    /* cout << endl; */
+
+    cout << "pais -> ";
+    for (int i = 1; i <= n; i++) cout << daddy[i] << " ";
     cout << endl;
 
 }
