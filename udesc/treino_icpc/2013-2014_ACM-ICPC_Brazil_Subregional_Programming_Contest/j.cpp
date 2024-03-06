048660dbd02e0985576cbcaf1cb74b527f799f35
leonardonm15 <leonardonm15@gmail.com>
2024-03-05 21:56:06 -0300

    aprendendo mais coisa

--- udesc/treino_icpc/2013-2014_ACM-ICPC_Brazil_Subregional_Programming_Contest/j.cpp
+++ udesc/treino_icpc/2013-2014_ACM-ICPC_Brazil_Subregional_Programming_Contest/j.cpp
@@ -10,34 +10,63 @@ using namespace std;
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
 
-
 const int N = 2e4 + 5;
 const int M = 1e5 + 5; 
+const int lg = log2(N) + 1;
 vector<pii> adj[N];
-
 vector<int> sz(N);
 vector<int> daddy(N);
 vector<int> vis(N);
-vector<vector<int>> st(N, vector<int>(log(N)));
+vector<int> tin(N);
+vector<int> tout(N);
+vector<vector<int>> up(N, vector<int>(log));
+vector<vector<int>> st(N, vector<int>(log));
+
+bool ancestor(int u, int v) {
+    if (tin[u] <= tin[v] && tout[u] >= tout[v]) return true;
+}
+
+int lca(int u, int v) {
+    int d;
+    if (tin[v] > tin[u]) {
+        d = tin[v] - tin[u];
+        for (int i=32; i > 0; i--) {
+            i >>= 1;
+            if (d & i) {
+                u = up[u][i];
+            }
+        }
+    } else if (tin[u] > tin[v]) {
+        d = tin[u] - tin[v];
+    }
+}
 
 int c = 0;
 int find(int a) {
     return (daddy[a] == a ? a : daddy[a] = find(daddy[a]));
 }
 
-void dfs(int u, int p, int d) {
-    st[u][0] = u;
-    for (int j = 1; (1 << j) <= d; j++) { 
-        st[u][j] = st[st[u][j - 1]][j - 1]
+void dfs(int u, int p, int w) {
+    up[u][0] = p;
+    vis[u]++;
+    tin[u] = t++;
+    for (int j = 1; (1 << j) <= lg; j++) {
+        up[u][j] = up[up[p][j - 1]][j - 1];
     }
-    for (auto cara: adj[u]) {
+
+    for (auto [w, v]: adj[u]) {
+        if (!vis[v]) {
+            dfs(v, u, w);
+        }
     }
+
+    tout[u] = t++;
 }
 
 void solve () {
     int n, m, q; cin >> n >> m >> q;
     // teste
-    vector<tuple<int, int, int>> edg;
+    vector<tuple<int, int, int, int>> edg;
     for (int i=0; i <= n; i++) {
         daddy[i] = i;
         st[i][0] = 0;
@@ -46,7 +75,7 @@ void solve () {
 
     for (int i=0; i < m; i++) {
         int a, b, w; cin >> a >> b >> w;
-        edg.push_back({w, a, b});
+        edg.push_back({w, a, b, 0});
     }
 
     sort(edg.rbegin(), edg.rend());
@@ -62,7 +91,7 @@ void solve () {
             sz[a] += sz[b];
             adj[a].push_back({b, w});
             adj[b].push_back({a, w});
-	}
+        }
     }
     /* cout << endl; */
 
