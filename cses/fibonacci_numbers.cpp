9c9bd26af3666a71b991d4dc44e0df7028e16025
leonardonm15 <leonardonm15@gmail.com>
2024-03-11 20:51:18 -0300

    tentando implementar multiplicação de  matriz

---
+++ cses/fibonacci_numbers.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n' 
+#define int long long
+#define mat vector<vector<int>>;
+
+// ----------    GRIND MENTALITY    ---------     
+// /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
+// ----------    GRIND MENTALITY    ---------     
+
+mat operator*(mat a, mat b) {
+
+}
+
+mat base = {
+    {1, 0, 0, 0},
+    {0, 0, 1, 0},
+    {0, 0, 0, 1},
+    {2, 1, 2, 3}
+};
+
+void solve () {
+    int n; cin >> n;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if (TC){
+        cin >> TC;
+        while (TC--) solve();
+    } else {
+        solve();
+    }
+    return 0;
+}
