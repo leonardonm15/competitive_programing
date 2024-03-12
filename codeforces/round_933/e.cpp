7ec96391d5f23994487b91a856f7a8196df8079b
leonardonm15 <leonardonm15@gmail.com>
2024-03-12 17:44:16 -0300

    tomando pra e do div 3

--- codeforces/round_933/e.cpp
+++ codeforces/round_933/e.cpp
@@ -10,7 +10,28 @@ using namespace std;
 // ----------    GRIND MENTALITY    ---------     
 
 void solve () {
-    
+    int m, n, k, d; cin >> m >> n >> k >> d;
+    vector<int> resps;
+    multiset<int> mn;
+    vector<int> dp(n);
+    dp[0] = 1;
+    dp[n - 1] = 1;
+
+    for (int i=0; i < m; i++) {
+        vector<int> arr(n);
+        for (int i=0; i < n; i++) cin >> arr[i];
+        // custo do meu cara + custo pra chegar no menor cara que eu alcanço
+        for (int i=0; i < d; i++) {
+            mn.emplace(arr[i]);
+        }
+
+        for (int i=0; i < n; i++) {
+            mn.emplace(arr[i]);
+            if (mn.size() > d) mn.erase(arr[i - d]);
+
+            
+        }
+    }
 }
 
 signed main() {
