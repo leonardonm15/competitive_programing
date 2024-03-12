d2d62aba94dc28cc488cd67cd6b0c08afd10bf73
leonardonm15 <leonardonm15@gmail.com>
2024-03-11 23:59:24 -0300

    fib sequence com recorrencia

--- cses/fibonacci_numbers.cpp
+++ cses/fibonacci_numbers.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define endl '\n' 
 #define int long long
-#define mat vector<vector<int>>;
+#define mat vector<vector<int>>
 
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
@@ -28,22 +28,45 @@ mat id {
 };
 
 mat operator*(mat a, mat b) {
-    mat resp(b.size(), vector<int> b[0].size());
+    mat resp(a[0].size(), vector<int>(b.size()));
     for (int i=0; i < a.size(); i++) {
-        for (int j = 0; j < a[i].size(); j++) {
-            resp[i][0] += (a[i][j] * b[j][0]) % mod;
+        for (int j=0; j < b[0].size(); j++) {
+            for (int k=0; k < b.size(); k++) {
+                resp[i][j] += a[i][k] * b[k][j] % mod;
+                resp[i][j] %= mod;
+            }
         }
+    }
+    return resp;
+}
+
+mat operator^(mat a, int k) {
+    mat resp = id; 
+    while (k) {
+        if (k & 1) {
+            resp = resp * a;
+        }
+        a = a * a;
+        k >>= 1;
     }
     return resp;
 }
 
 void solve () {
     int n; cin >> n;
+    if (n == 0) cout << 0 << endl;
+    else if (n == 1 || n == 2) cout << 1 << endl;
+    else {
+        mat resp = prog ^ n;
+        resp = resp * base;
+        cout << resp[0][0] << endl;
+        /* cout << resp[1][0] << endl; */
+    }
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int TC = 1;
+    int TC = 0;
     if (TC){
         cin >> TC;
         while (TC--) solve();
