14f09af7efc43061765e838a903b07dc2b4f63a7
leonardonm15 <leonardonm15@gmail.com>
2024-04-03 21:56:47 -0300

    desaprendi

--- atcoder/dp_educational/redo/frog1.cpp
+++ atcoder/dp_educational/redo/frog1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 #define endl '\n' 
-//#define int long long
+#define int long long
 
 // ----------    GRIND MENTALITY    ---------     
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
@@ -11,6 +11,26 @@ using namespace std;
 
 void solve () {
     int n; cin >> n;
+    vector<int> arr(n);
+    vector<int> dp(n);
+    for (int i=0; i < n; i++) cin >> arr[i];
+    for (int i=1; i < n; i++) {
+        int j1 = 1e9 + 5;
+        int j2 = 1e9 + 5;
+        if (i - 1 >= 0) j1 = dp[i - 1] + abs(arr[i] - dp[i - 1]);
+        if (i - 2 >= 0) j2 = dp[i - 2] + abs(arr[i] - dp[i - 2]);
+        dp[i] += min(j1, j2);
+    }
+
+    cout << "arr -> ";
+    for (auto cara: arr) cout << cara << " ";
+    cout << endl;
+
+    cout << "dp-> ";
+    for (auto cara: dp) cout << cara << " ";
+    cout << endl;
+
+    /* cout << dp[n - 1] + arr[n - 1] << endl; */
 }
 
 signed main() {
