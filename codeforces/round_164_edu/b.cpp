797321755335dd4a34710eda6c8762020d11874d
leonardonm15 <leonardonm15@gmail.com>
2024-04-14 22:12:11 -0300

    questaozinha chata

--- codeforces/round_164_edu/b.cpp
+++ codeforces/round_164_edu/b.cpp
@@ -9,10 +9,17 @@ using namespace std;
 // /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
 // ----------    GRIND MENTALITY    ---------     
 
+int tc = 0;
 void solve () {
+    tc++;
     int n; cin >> n;
     vector<int> arr(n);
     for (int i=0; i < n; i++) cin >> arr[i];
+    /* if (tc == 824) { */
+    /*     cout << n << "#"; */
+    /*     for (auto cara: arr) cout << cara << "$"; */
+    /*     cout << endl; */
+    /* } */
     int k = 0;
     for (int i=0; i < n; i++) {
         if (arr[i] != arr[n - 1]) {
@@ -28,11 +35,32 @@ void solve () {
         }
     }
 
-    cout << (k == 0 ? -1: k) << endl;
+    vector<int> pos;
+    if (arr.size() >= 3) {
+        for (int i=1; i < n - 1; i++) {
+            if (arr[i] != arr[0]) {
+                pos.push_back(i);
+            }
+        }
+    }
+
+    int k2 = 1e9 + 5;
+    if (pos.size() >= 2) {
+        for (int i=1; i < (int) pos.size(); i++) {
+            k2 = min(k2, pos[i] - pos[i - 1] - 1);
+        }
+    }
+
+    if (k == 0) {
+        cout << -1 << endl;
+        return;
+    }
+
+    cout << min(k, k2) << endl;
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    /* ios_base::sync_with_stdio(0);cin.tie(0); */
     int TC = 1;
     if (TC){
         cin >> TC;
