c5769ec1a0d8d0ae00745b7a98e65bec208f0b02
leonardonm15 <leonardonm15@gmail.com>
2024-04-24 16:34:59 -0300

    fiz booooomba

--- udesc/bruteclass/maratonando_4/b.cpp
+++ udesc/bruteclass/maratonando_4/b.cpp
@@ -4,28 +4,45 @@ using namespace std;
 
 #define endl '\n' 
 #define int long long
-vector<bool> sv;
 
-void solve () {
-    int n; cin >> n;
-    vector<int> arr(n);
-    set<int> set;
-    for (int i=0; i < n; i++) {
-        cin >> arr[i];
-        set.emplace(arr[i]);
-    }
+vector<vector<int>> gcds(1e3 + 5, vector<int>(1e3 + 5, 0));
 
-    for (auto num: set) {
-        for (int i=0; i < n; i++) {
-            if (arr[i]
+void calc() {
+    for (int i=1; i <= 1e3 + 2; i++) {
+        for (int j=1; j <= 1e3 + 2; j++) {
+            gcds[i][j] = gcd(i, j);
         }
     }
 }
 
+
+void solve () {
+    int n; cin >> n; 
+    vector<int> arr(n);
+    vector<int> freq(1e4, -1);
+
+    for (int i=0; i < n; i++) {
+        int num; cin >> num;
+        arr[i] = num;
+        freq[num] = i;
+    }
+
+    int resp = -1;
+    for (int i=0; i < n; i++) {
+        for (int j=1; j < 1e3 + 2; j++) {
+            if (freq[j] > -1 && gcds[arr[i]][j] == 1) {
+                resp = max(resp, i + freq[j] + 2);
+            }
+        }
+    }
+
+    cout << resp << endl;
+}
+
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    sv = sieve(1005);
     int TC = 1;
+    calc();
     if (TC){
         cin >> TC;
         while (TC--) solve();
