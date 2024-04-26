485746f4201ee107c79bf1eb99367a65c1888f81
leonardonm15 <leonardonm15@gmail.com>
2024-04-26 20:55:27 -0300

    to mt feliz

--- codeforces/round_833/c.cpp
+++ codeforces/round_833/c.cpp
@@ -5,51 +5,10 @@ using namespace std;
 #define endl '\n' 
 #define int long long
 
-// ----------    GRIND MENTALITY    ---------     
-// /* ESCREVE TODAS AS SUAS IDEIAS E TESTA */
-// ----------    GRIND MENTALITY    ---------     
-
-bool check(pair<int, int> a, pair<int, int> b) {
-    return (a.first == b.first ? a.second > b.second : a.first > b.first);
-}
-
 void solve () {
     int n; cin >> n;
     vector<int> arr(n);
-    for (int i=0; i < n; i++) cin >> arr[i];
-    vector<int> pref;
-    vector<pair<int, int>> spref(n);
-    set<int> zero;
-    int sum = 0;
-    for (int i=0; i < n; i++) {
-        if (arr[i] == 0) zero.emplace(i);
-        sum += arr[i];
-        pref[i] = sum;
-        spref[i] = {sum, i}
-    }
-    // sorta os caras do pair<int, int> mas os caras de indices mais a direita
-    // ficam mais a direita quando sortados os valores forem iguais
-
-    sort(spref.begin(), spref.end(), check);
-
-    for (int i=0; i < n; i++) {
-        int l = i;
-        int r = n - 1;
-        int tg = pref[i] - arr[i];
-        // tenho que achar um pref[i] tal que pref[i] - tg  = -tg
-        while (l <= r) {
-            int mid = (l + r) >> 1;
-            if (spref[mid] 
-
-        }
-        // pega o meu cara atual no arr
-        // diminui ele do pref atual
-        // pra soma do range dar 0 
-        //o prefixo tem que ser pref[j] - (pref[i] - arr[i]) == -arr[i]
-
-
-    }
-
+    for (int i=0; i < n; i++) 
 
 }
 
