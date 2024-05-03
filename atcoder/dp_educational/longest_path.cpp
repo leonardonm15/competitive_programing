a5540eb4f22ee285707d9ef55ad48ddd2ced5772
leonardonm15 <leonardonm15@gmail.com>
2024-05-03 18:44:20 -0300

    segundo bomba

--- atcoder/dp_educational/longest_path.cpp
+++ atcoder/dp_educational/longest_path.cpp
@@ -10,15 +10,17 @@ vector<int> adj[N];
 vector<int> vis(N);
 vector<pair<int, int>> tin(N);
 vector<int> dp(N, 0);
-int resp = 0;
 
 void dfs(int u) {
     vis[u]++;
+    int mxc = 0;
     for (auto v: adj[u]) {
         if (!vis[v]) dfs(v);
-        dp[u] = max(dp[u], dp[u] + dp[v]);
+        mxc = max(mxc, dp[v]);
     }
-    resp = max(resp, dp[u]);
+
+    dp[u] += mxc;
+    return;
 }
 
 void solve () {
@@ -27,20 +29,26 @@ void solve () {
     for (int i=0; i <= n; i++) {
         tin[i].second = i + 1;
     }
-    for (int i=0; i < n; i++) {
+    for (int i=0; i < m; i++) {
         int u, v; cin >> u >> v;
         adj[u - 1].push_back(v - 1);
         tin[v].first++;
     }
 
     sort(tin.begin(), tin.begin() + n);
-
+    int resp = 0;
+    for (int i=0; i < n; i++) {
+        if (!tin[i].first) {
+            dfs(i);
+            resp = max(resp, dp[i]);
+        }
+    }
     cout << resp << endl;
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int TC = 1;
+    int TC = 0;
     if (TC){
         cin >> TC;
         while (TC--) solve();
--- codeforces/round_943/f.cpp
+++ codeforces/round_943/f.cpp
@@ -5,7 +5,63 @@ using namespace std;
 #define endl '\n' 
 #define int long long
 
-struct 
+struct merge_sort_tree { // separate_shuffle_tornado
+    vector<vector<pair<int, int>>> mseg;
+    int n;
+
+    vector<pair<int, int>> merge(vector<pair<int, int>> a, vector<pair<int, int>> b) {
+        int x = 0;
+        int y = 0;
+        vector<pair<int, int>> c;
+
+        while (x < a.size() || y < b.size()) {
+            if (x < a.size() && y < b.size()) {
+                if (a[x].first < b[y].first) c.push_back(a[x++]);
+                if (a[x].first == b[y].first) {
+                    if (a[x].second < b[y].second) c.push_back((((((a[x++]))))));
+                    else c.push_back(b[y++]);
+                }
+                else c.push_back(b[y++].first);
+            }
+
+            if (x < a.size() && y >= b.size()) {
+                c.push_back(a[x++]);
+            }
+
+            if (y < b.size() && x >= a.size()) {
+                c.push_Back(b[y++]);
+            }
+        }
+
+        return c;
+    }
+
+    void query(int u, int l, int r, int L, int R, int k) {
+        
+    }
+    void query(int l, int r, int k) {
+        query(1
+
+    }
+
+    void build(int u, int l, int r, vector<int>& arr) {
+        if (l == r) {
+            mseg[u].push_back(arr[l]);
+            return;
+        }
+
+        int mid = (l + r) >> 1;
+        build(2 * u, l, mid, arr);
+        build(2 * u + 1, mid + 1, r, arr);
+        mseg[u] = merge(mseg[2 * u], mseg[2 * u + 1]);
+    }
+
+    void build(vector<int>& arr) {
+        n = arr.size();
+        mseg.resize(4 * n);
+        build(1, 0, n - 1, arr);
+    }
+}
 
 
 
