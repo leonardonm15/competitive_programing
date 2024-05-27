0b9d1071631762857f3f9af21da62d1564a6a4d8
leonardonm15 <leonardonm15@gmail.com>
2024-05-27 20:00:04 -0300

    aprendendo mais coisa

--- icpc/2024_ICPC_Gran_Premio_de_Mexico_1ra_Fecha/a.cpp
+++ icpc/2024_ICPC_Gran_Premio_de_Mexico_1ra_Fecha/a.cpp
@@ -1,36 +1,38 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+#define int long long
 const int N = 1e4 + 5;
+
 int dp[N][N];
+int arr[N];
 
 void calc(int n) {
-    for (int i=1; i <= n; i++) {
-        int m = 1e9;
-        for (int j=i + 1; j <= n; j++) {
-            if (i > 1) m = min(abs(dp[0][i] - dp[0][j]), dp[i - 1][j]);
-            else m = min(m, abs(dp[0][i] - dp[0][j]));
-            dp[i][j] = m;
-            cout << dp[i][j] << " ";
+    for (int i = 1; i < n; i++) dp[i - 1][i] = abs(arr[i - 1] - arr[i]);
+
+    for (int k=2; k < n; k++) {
+        for (int i = 1; i < n; i++) {
+            if (i < k) continue;
+            dp[i - k][i] = min({dp[i - k][i - 1], dp[i - k + 1][i], abs(arr[i - k] - arr[i])});
         }
-        cout << endl;
     }
 }
 
 void solve() {
     int n; cin >> n;
-    for (int i=1; i <= n; i++) cin >> dp[0][i];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
     calc(n);
+
     int q; cin >> q;
-    while(q--) {
+    while (q--) {
         int l, r; cin >> l >> r;
-        cout << dp[l][r] << endl;
+        cout << dp[l - 1][r - 1] << endl;
     }
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
     solve();
-
+    return 0;
 }
