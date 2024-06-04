1bb707be96b96ddb0c740354b253acc8f90bd594
leonardonm15 <leonardonm15@gmail.com>
2024-06-04 19:15:26 -0300

    maratonando

--- codeforces/round_950/d.cpp
+++ codeforces/round_950/d.cpp
@@ -9,65 +9,68 @@ void solve () {
     int n; cin >> n;
     vector<int> arr(n);
     vector<int> gcds;
-    for (int i=0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    for (int i= 1; i < n; i++) {
-        gcds.push_back(gcd(arr[i], arr[i - 1]));
-    }
+    for (int i= 1; i < n; i++) gcds.push_back(gcd(arr[i], arr[i - 1]));
+
+    cout << "gcds -> ";
+    for (auto c: gcds) cout << c << " ";
+    cout << endl;
+
     
     vector<int> points;
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < (int)gcds.size(); i++) {
         if (gcds[i] < gcds[i - 1]) {
             points.push_back(i);
         }
     }
 
-    int mud = 0;
-    for (int i=1; i < n - 2; i++) {
+    bool flag = false;
+    for (int i = 1; i < (int)gcds.size() - 1; i++) {
         if (gcds[i] < gcds[i - 1]) {
-            /* cout << "gcds[i] -> " << gcds[i] << endl; */
-            /* cout << "gcds[i - 1] -> " << gcds[i - 1] << endl; */
-            /* cout << "arr[i] -> " << arr[i] << endl; */
-            /* cout << "arr[i - 1] -> " << arr[i - 1] << endl; */
+            flag = true;
+        }
+    }
+
+    if (!flag) {
+        cout << "YES" << endl;
+        return;
+    }
+
+    flag = false;
+    for (int i = 2; i < (int)gcds.size(); i++) {
+        if (gcds[i] < gcds[i - 1]) flag = true;
+    }
+
+    if (!flag) {
+        /* cout << "opa 2 -> " << endl; */
+        cout << "YES" << endl;
+        return;
+    }
+
+    int mud = 0;
+    cout << "bomba -> ";
+    for (int i = 1; i < n - 2; i++) {
+        if (gcds[i] < gcds[i - 1]) {
             int num = gcd(arr[i], arr[i + 2]);
-            /* cout << "num -> " << num << endl; */
-            if (num > gcds[i]) {
-                gcds[i] = num;
-                gcds[i + 1] = num;
-                mud++;
-            }
+            gcds[i] = num;
+            gcds[i + 1] = num;
+            mud++;
         }
+        cout << gcds[i] << " ";
     }
-
-    cout << "gcds -> ";
-    for (auto c: gcds) cout << c << " ";
     cout << endl;
-    
 
-    if (mud == 0 && points.size() > 0) {
-        bool flag = false;
-        for (int i=1; i < gcds.size() - 1; i++) {
+    if (mud > 1) cout << "NO" << endl;
+    else {
+        for (int i = 1; i < (int)gcds.size(); i++) {
             if (gcds[i] < gcds[i - 1]) {
-                flag = true;
+                cout << "NO" << endl;
+                return;
             }
         }
-
-        if (!flag) {
-            cout << "YES" << endl;
-            return;
-        }
-        flag = true;
-
-        for (int i=2; i < gcds.size(); i++) {
-            if (gcds[i] < gcds[i - 1]) 
-        }
-        if (!flag) {
-            cout << "YES" << endl;
-            return;
-        }
+        cout << "YES" << endl;
     }
-    if (mud > 2) cout << "NO" << endl;
-    else cout << "MAYBE" << endl;
 }
 
 signed main() {
