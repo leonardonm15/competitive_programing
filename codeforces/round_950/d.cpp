b12fae0a15d61ed2d4802b2f21b5dc8b74047f75
leonardonm15 <leonardonm15@gmail.com>
2024-06-10 17:45:00 -0300

    mais bomba

--- codeforces/round_950/d.cpp
+++ codeforces/round_950/d.cpp
@@ -13,11 +13,10 @@ void solve () {
 
     for (int i= 1; i < n; i++) gcds.push_back(gcd(arr[i], arr[i - 1]));
 
-    cout << "gcds -> ";
-    for (auto c: gcds) cout << c << " ";
-    cout << endl;
+    /* cout << "gcds -> "; */
+    /* for (auto c: gcds) cout << c << " "; */
+    /* cout << endl; */
 
-    
     vector<int> points;
     for (int i = 1; i < (int)gcds.size(); i++) {
         if (gcds[i] < gcds[i - 1]) {
@@ -25,52 +24,76 @@ void solve () {
         }
     }
 
-    bool flag = false;
-    for (int i = 1; i < (int)gcds.size() - 1; i++) {
-        if (gcds[i] < gcds[i - 1]) {
-            flag = true;
-        }
-    }
-
-    if (!flag) {
+    /* cout << "Points.size() -> " << points.size() << endl; */
+    if (!points.size()) {
         cout << "YES" << endl;
         return;
-    }
-
-    flag = false;
-    for (int i = 2; i < (int)gcds.size(); i++) {
-        if (gcds[i] < gcds[i - 1]) flag = true;
-    }
-
-    if (!flag) {
-        /* cout << "opa 2 -> " << endl; */
-        cout << "YES" << endl;
-        return;
-    }
-
-    int mud = 0;
-    cout << "bomba -> ";
-    for (int i = 1; i < n - 2; i++) {
-        if (gcds[i] < gcds[i - 1]) {
-            int num = gcd(arr[i], arr[i + 2]);
-            gcds[i] = num;
-            gcds[i + 1] = num;
-            mud++;
-        }
-        cout << gcds[i] << " ";
-    }
-    cout << endl;
-
-    if (mud > 1) cout << "NO" << endl;
-    else {
-        for (int i = 1; i < (int)gcds.size(); i++) {
-            if (gcds[i] < gcds[i - 1]) {
-                cout << "NO" << endl;
-                return;
+    } else if (points.size() == 1 || points.size() == 2) {
+        for (auto pt: points) {
+            vector<int> a1, a2, a3, g1, g2, g3;
+            bool f1 = true, f2 = true, f3 = true;
+            for (int i=0; i < n; i++) {
+                if (i == pt - 1) {
+                    a2.push_back(arr[i]);
+                    a3.push_back(arr[i]);
+                }
+                else if (i == pt) {
+                    a1.push_back(arr[i]);
+                    a3.push_back(arr[i]);
+                }
+                else if (i == pt + 1) {
+                    a1.push_back(arr[i]);
+                    a2.push_back(arr[i]);
+                } else {
+                    a1.push_back(arr[i]);
+                    a2.push_back(arr[i]);
+                    a3.push_back(arr[i]);
+                }
             }
+
+            for (int i=1; i < (int)a1.size(); i++) {
+                g1.push_back(gcd(a1[i], a1[i - 1]));
+                g2.push_back(gcd(a2[i], a2[i - 1]));
+                g3.push_back(gcd(a3[i], a3[i - 1]));
+            }
+
+            /* cout << "a1 -> "; */
+            /* for (auto c: a1) cout << c << " "; */
+            /* cout << endl; */
+
+            /* cout << "g1 -> "; */
+            /* for (auto c: g1) cout << c << " "; */
+            /* cout << endl; */
+
+            /* cout << "a2 -> "; */
+            /* for (auto c: a2) cout << c << " "; */
+            /* cout << endl; */
+
+            /* cout << "g2 -> "; */
+            /* for (auto c: g2) cout << c << " "; */
+            /* cout << endl; */
+
+            /* cout << "a3 -> "; */
+            /* for (auto c: a3) cout << c << " "; */
+            /* cout << endl; */
+
+            /* cout << "g3 -> "; */
+            /* for (auto c: g3) cout << c << " "; */
+            /* cout << endl; */
+
+            for (int i = 1; i < (int) g1.size(); i++) {
+                if (g1[i] < g1[i - 1]) f1 = false;
+                if (g2[i] < g2[i - 1]) f2 = false;
+                if (g3[i] < g3[i - 1]) f3 = false;
+            }
+
+            if ((f1 | f2 | f3) == false) cout << "NO" << endl;
+            else cout << "YES" << endl;
+            return;
         }
-        cout << "YES" << endl;
     }
+
+    cout << "NO" << endl;
 }
 
 signed main() {
