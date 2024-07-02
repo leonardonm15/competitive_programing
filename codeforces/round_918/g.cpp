428ce4f54528f3e209e5bbbd19b4a7d7e1241d73
leonardonm15 <leonardonm15@gmail.com>
2024-07-01 23:29:20 -0300

    gzinha de div 4

--- codeforces/round_918/g.cpp
+++ codeforces/round_918/g.cpp
@@ -1,69 +1,65 @@
 #include <bits/stdc++.h>
 
-# define int long long
 using namespace std;
-const int maxn = 1000 + 5;
-const int inf = 2e17;
-vector<pair<int, int>> adj[maxn];
-vector<vector<int>> dist(maxn, vector<int>(maxn, inf));
-vector<int> bikes(maxn);
-vector<int> bike_nodo(maxn, inf);
 
-void djks(int n, int bike) {
-    priority_queue< pair<int, int>, 
-                    vector<pair<int, int>>, 
-                    greater<pair<int, int>> > pq;
-    dist[n][bike] = 0;
-    pq.emplace(0, n);
-    bike_nodo[n] = bikes[n];
-    while(!pq.empty()) {
-        int peso = pq.top().first;
-        int nodo = pq.top().second;
-        int k = min(bike_nodo[nodo], bikes[nodo]);
+#define endl '\n' 
+#define int long long
+#define pii pair<int, int>
+#define tpi tuple<int, int, int>
+
+const int N = 1e3 + 5;
+const int INF = 1e18;
+vector<pii> adj[N]; // w, v
+int dist[N][N];
+int bike[N];
+
+int djks(int root, int n) {
+    priority_queue<tpi, vector<tpi>, greater<tpi>> pq;
+    dist[root][bike[root]] = 0;
+    pq.push({0, root, bike[root]});
+
+    while (pq.size()) {
+        auto [d, u, s] = pq.top();
         pq.pop();
-        for (auto [w, cara]: adj[nodo]) {
-            if (dist[nodo] + (w * k) < dist[cara]) {
-                dist[cara] = dist[nodo] + (w * k);
-                int b = min(k, bikes[cara]);
-                pq.emplace(make_pair(dist[cara], cara));
-                pq.emplace(make_pair(dist[cara] + (dist[nodo] + (w * b)), nodo));
-                bike_nodo[nodo] = b;
-                /* bike_nodo[cara] = k; */
+        if (d > dist[u][s]) continue;
+        for (auto [w, v]: adj[u]) {
+            int nd = d + (s * w);
+            if (dist[v][s] > nd) {
+                dist[v][s] = nd;
+                if (bike[v] < s) pq.push({nd, v, bike[v]});
+                else pq.push({nd, v, s});
             }
         }
     }
+    
+    int resp = 1e18;
+    for (int i = 1; i < N; i++) resp = min(resp, dist[n][i]);
+
+    return resp;
 }
 
 void solve () {
     int n, m; cin >> n >> m;
-    for (int i=0; i < m; i++) {
-        int a, b, c; cin >> a >> b >> c;
-        adj[a].push_back({c, b});
-        adj[b].push_back({c, a});
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+        adj[v].push_back({w, u});
     }
 
-    for (int i=1; i <= n; i++) cin >> bikes[i];
-    bike_nodo[1] = bikes[1];
-    djks(1, 1);
-
-    int resp = inf;
-    for (int i=0; i <= n; i++) {
-        resp = min(resp, dist[n][i]);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) dist[i][j] = INF;
     }
 
-    for (int i=0; i <= n; i++) {
-        adj[i].clear();
-        bike_nodo[i] = inf;
-        for (int j=0; i <= n; j++) {
-            dist[i][j] = inf;
-        }
-        bikes[i] = 0;
-    }
+    for (int i = 1; i <= n; i++) cin >> bike[i];
 
+    cout << djks(1, n) << endl;
+
+    for (int i = 0; i <= n; i++) adj[i].clear();
 }
 
 signed main() {
-    // ios_base::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);cin.tie(0);
     int TC = 1;
     if (TC){
         cin >> TC;
@@ -73,4 +69,3 @@ signed main() {
     }
     return 0;
 }
-
