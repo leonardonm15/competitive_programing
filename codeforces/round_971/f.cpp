75c9af936ad931a15d8cd8fbb897b48378591a23
leonardonm15 <leonardonm15@gmail.com>
2024-09-03 21:27:40 -0300

    continuação

---
+++ codeforces/round_971/f.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n' 
+#define int long long
+
+void solve () {
+    int n, q; cin >> n >> q;
+    vector<int> arr(n);
+
+    int sum = 0;
+    vector<int> pref;
+
+    pref.push_back(sum);
+    for (int i = 0; i < n; i++) {
+        int num; cin >> num;
+        int sum += num;
+        pref.push_back(num);
+        arr[i] = num;
+    }
+
+
+    while (q--) {
+        int l, r; cin >> l >> r;
+        l--; r--;
+
+        int 
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int TC = 1;
+    if (TC){
+        cin >> TC;
+        while (TC--) solve();
+    } else {
+        solve();
+    }
+    return 0;
+}
