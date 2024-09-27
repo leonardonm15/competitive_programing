1ac9450f2afcc0ea7bd1045d06a2f3067496d5ec
leonardonm15 <leonardonm15@gmail.com>
2024-09-27 01:45:11 -0300

    mais coisa da e

--- codeforces/round_974/e2.cpp
+++ codeforces/round_974/e2.cpp
@@ -13,30 +13,46 @@ const int INF = 1e18;
 
 vector<pii> adj[N];
 int dist[2][N];
+int ndist[2][N];
 int horse[N];
 
-void djks(int root) {
-    dist[horse[root]][root] = 0;
-    priority_queue<tii> pq; // distancia_atual nodo quantidade de hs
-    pq.push({dist[horse[root]][root], root, horse[root]});
+void clear_tc(int n) {
+    for (int i = 0; i <= n; i++) {
+        adj[i].clear();
+        dist[0][i] = INF;
+        dist[1][i] = INF;
+        ndist[0][i] = INF;
+        ndist[1][i] = INF;
+    }
+}
 
+void djks(int root, int dist[2][N]) {
+    dist[horse[root]][root] = 0;
+    priority_queue<tii> pq; // distancia atual, nodo, estou montado ?
+    pq.push({dist[horse[root]][root], root, horse[root]});
+    
     while (!pq.empty()) {
-        auto [d, u, cav] = pq.top();
-        cout << "------------------" << endl;
-        cout << "d -> " << d << endl;
-        cout << "u -> " << u << endl;
-        cout << "cav -> " << cav << endl;
+        auto [d, u, montado] = pq.top();
+
+        if (!montado && horse[u]) {
+            dist[1][u] = d;
+            montado = 1;
+        }
+
         pq.pop();
 
-        if (dist[cav][u] < d) continue;
+        if (dist[montado][u] < d) continue;
 
         for (auto [v, w]: adj[u]) {
-            if (d + w < dist[cav][v]) {
-                dist[cav | horse[v]][v] = d + (cav ? w/2 : w);
-                pq.push({dist[cav | horse[v]][v], v, (cav | horse[v])});
+
+            if (dist[montado][v] > d + w) {
+                int k = d + (montado ? w / 2 : w);
+                dist[montado][v] = k;
+                pq.push({k, v, montado});
             }
         }
     }
+
 }
 
 void solve () {
@@ -45,6 +61,8 @@ void solve () {
     for (int i = 0; i <= n; i++) {
         dist[0][i] = INF;
         dist[1][i] = INF;
+        ndist[0][i] = INF;
+        ndist[1][i] = INF;
     }
 
 
@@ -59,7 +77,8 @@ void solve () {
         adj[v].push_back({u, w});
     }
 
-    djks(1);
+    djks(1, dist);
+    djks(n, ndist);
 
     cout << "dist -> ";
     for (int i = 1; i <= n; i++) { cout << dist[0][i] << " "; }
@@ -67,15 +86,30 @@ void solve () {
     for (int i = 1; i <= n; i++) { cout << dist[1][i] << " "; }
     cout << endl;
 
+    cout << "-----------" << endl;
+
+    cout << "ndist -> ";
+    for (int i = 1; i <= n; i++) cout << ndist[0][i] << " ";
+    cout << endl;
+    for (int i = 1; i <= n; i++) cout << ndist[1][i] << " ";
+    cout << endl;
+
     if (dist[0][n] == INF && dist[1][n] == INF) {
         cout << -1 << endl;
+        clear_tc(n);
         return;
     } 
+
+    /* int resp = 1e18; */
+    /* for (int i = 1; i <= n; i++) { */
+    /*     int d = dist[1][i] */
+    /*     min(resp, */ 
+    /* } */
     
 }
 
 signed main() {
-    /* ios_base::sync_with_stdio(0);cin.tie(0); */
+    ios_base::sync_with_stdio(0);cin.tie(0);
     int TC = 1;
     if (TC){
         cin >> TC;
