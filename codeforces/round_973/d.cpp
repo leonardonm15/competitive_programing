501eb32c33c3206db3546d1d719a15cf6b5657d4
leonardonm15 <leonardonm15@gmail.com>
2024-09-28 13:28:08 -0300

    opa

--- codeforces/round_973/d.cpp
+++ codeforces/round_973/d.cpp
@@ -7,10 +7,45 @@ using namespace std;
 
 void solve () {
     int n; cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
+    vector<int> vec(n);
 
+    int mx = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+        mx = max(mx, vec[i]);
     }
+
+    int l = 1;
+    int r = 1e12;
+    int ans = 1e18;
+
+    for (int k = 1; k <= mx; k++) {
+        vector<int> arr = vec;
+        int resto = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= k) {
+                resto += arr[i] - k;
+                arr[i] = k;
+            } else {
+                int a = min(resto, k - arr[i]);
+                arr[i] += a;
+                resto -= a;
+            }
+        }
+
+        if (resto > 0) arr[n - 1] += resto;
+        int minimo = 1e18;
+        int maximo = 0;
+
+        for (auto c: arr) {
+            minimo = min(minimo, c);
+            maximo = max(maximo, c);
+        }
+
+        cout << "k - " << k << " -> " << maximo - minimo << endl;
+    }
+
 }
 
 signed main() {
