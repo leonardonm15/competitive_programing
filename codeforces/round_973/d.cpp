bee5cb6b7c8c7ebfbe70e1a419febe2f1a87a043
leonardonm15 <leonardonm15@gmail.com>
2024-09-27 22:22:15 -0300

    tomei

--- codeforces/round_973/d.cpp
+++ codeforces/round_973/d.cpp
@@ -5,12 +5,84 @@ using namespace std;
 #define endl '\n' 
 #define int long long
 
-void solve () {
+const int INF = 1e18;
+
+int check(int mid, vector<int> arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > mid) {
+            int k = arr[i] - mid;
+            arr[i + 1] += k;
+            arr[i] -= k;
+        }
+    }
+
+
+    if (arr[n - 1] > mid) return -INF;
+
+    int resto = 0; 
+    int minimo = arr[0];
+    int sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        sum += arr[i];
+        if (arr[i] >= minimo) {
+            int k = arr[i] - minimo;
+            arr[i] -= k;
+            resto += k;
+        } else if (arr[i] + resto >= minimo) {
+            int k = (arr[i] + resto) - minimo; // quanto essa porra + resto > minimo
+            arr[i] = minimo;
+            resto -= k;
+        } else {
+            int k = sum / (i + 1);
+            resto = sum % (i + 1);
+            minimo = k;
+        }
+    }
+
+
+    /* if (arr[n - 1]> mid) return -INF; */
+    /* sort(arr.begin(), arr.end()); */
+
+    cout << "resto -> " << resto << endl;
+    cout << "arr -> ";
+    for (auto c: arr) cout << c << " ";
+    cout << endl;
+
+    if (resto > mid) return -INF;
+    return arr[n - 1] - minimo;
+}
+
+void solve() {
     int n; cin >> n;
     vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    
+    if (arr[0] > arr[n - 1]) {
+        int k = ((arr[0] + arr[n - 1]) + 1);
+        arr[0] = (arr[0] + arr[n - 1]) - (k / 2);
+        arr[n - 1] = k / 2;
     }
+
+    int l = 0;
+    int r = 1e12;
+    int ans = 1e18;
+
+    while (l <= r) {
+        int mid = (l + r) >> 1;
+        cout << "--------------------" << endl;
+        cout << "mid -> " << mid << endl;
+        int k = check(mid, arr);
+        if (k > -INF) {
+            cout << "k -> " << k << endl;
+            ans = min(ans, k);
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
 }
 
 signed main() {
