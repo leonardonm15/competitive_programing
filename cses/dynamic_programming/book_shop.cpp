bc6b9511df5fef04e7d7ca2190596422a00f0362
leonardonm15 <leonardonm15@gmail.com>
2024-10-04 21:35:53 -0300

    book shop passado

--- cses/dynamic_programming/book_shop.cpp
+++ cses/dynamic_programming/book_shop.cpp
@@ -3,24 +3,37 @@
 using namespace std;
 
 #define endl '\n' 
-#define int long long
+// #define int long long
 
-const int N = (1e3 * 1e3) + 5;
-const int K = 1e5 + 5;
-const int INF = 1e18;
+const int N = 1e3 + 5;
+const int X = 1e5 + 5;
 
-vector<vector<int>> dp(N, vector<int>(N, INF));
+int dp[N][X];
 
 void solve () {
     int n, k; cin >> n >> k;
 
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= k; j++) {
+            dp[i][j] = 0;
+        }
+    }
+
     vector<int> h(n), s(n);
     for (int i = 0; i < n; i++) cin >> h[i];
     for (int i = 0; i < n; i++) cin >> s[i];
+    for (int i = 1; i <= n; i++) {
+        int preço = h[i - 1];
+        int paginas = s[i - 1];
+        for (int j = 0; j <= k; j++) {
+            if (preço > j) { dp[i][j] = dp[i - 1][j]; }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - preço] + paginas);
+            }
+        }
+    }
 
-    vector<int> old(N, INF); // preço fica no valor da dp;
-    vector<int> now(N, INF); // 
-
+    cout << dp[n][k] << endl;
 }
 
 signed main() {
