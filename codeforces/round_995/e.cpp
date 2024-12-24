03181e823ece1f3d207211a561d5a5583ba055c3
leonardonm15 <leonardonm15@gmail.com>
2024-12-23 23:05:11 -0300

    passei a e

--- codeforces/round_995/e.cpp
+++ codeforces/round_995/e.cpp
@@ -4,49 +4,73 @@ using namespace std;
 
 #define endl '\n' 
 #define int long long
-#define pii pair<int, int>
 
-const int N = 2e5 + 5;
-
-vector<vector<int>> dp(N, vector<int>(3, -1));
-vector<int> a(N);
-vector<int> b(N);
-
-int resp = 0;
-
-int calc(int u, int val, int vp, int ks, int n, int k) {
-    if (ks > k) return -1;
-    if (u > n) return val;
-
-    if (dp[u][vp] != -1) return dp[u][vp];
-    
-    int v1 = calc(u + 1, val + a[u + 1], 0, ks    , n, k);
-    int v2 = calc(u + 1, val + b[u + 1], 1, ks + n, n, k);
-    int v3 = calc(u + 2, val           , 2, ks    , n, k);
-
-    return dp[u][vp] = max({v1, v2, v3});
+// Função c1
+bool c1(vector<int>& arr, int mid, int k) {
+    return arr[mid] >= k;
 }
 
-void solve () {
-    int n, k; cin >> n >> k;
+// Função de busca binária
+int bb(vector<int>& arr, int k, bool (*check)(vector<int>&, int, int)) {
+    int n = arr.size();
+    int l = 0;
+    int r = n - 1;
+    int ans = n;
 
+    while (l <= r) {
+        int mid = (l + r) >> 1;
 
-    for (int i = 1; i <= n; i++) cin >> a[i];
-    for (int i = 1; i <= n; i++) cin >> b[i];
-
-    cout << calc(0, 0, 0, 0, n, k) << endl;
-
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = -1;
-        dp[i][1] = -1;
-        dp[i][2] = -1;
+        if (check(arr, mid, k)) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
     }
+
+    return ans;
+}
+
+// Função solve
+void solve() {
+    int n, k; 
+    cin >> n >> k;
+
+    vector<int> a(n), b(n), sa(n), sb(n), all;
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    sa = a;
+    sb = b;
+
+    for (int i = 0; i < n; i++) {
+        all.push_back(a[i]);
+        all.push_back(b[i]);
+    }
+
+    sort(sa.begin(), sa.end());
+    sort(sb.begin(), sb.end());
+    sort(all.begin(), all.end());
+
+    int resp = -1;
+    for (auto val : all) {
+        int quantos_compraram = n - bb(sb, val, c1); // quantos caras pra direita
+        int good_review = n - bb(sa, val, c1);
+
+        if (quantos_compraram - good_review > k) continue;
+
+        resp = max(resp, quantos_compraram * val);
+    }
+
+    cout << resp << endl;
 }
 
 signed main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int TC = 1;
-    if (TC){
+    if (TC) {
         cin >> TC;
         while (TC--) solve();
     } else {
