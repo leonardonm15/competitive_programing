85054fce68a0b0f3f3e37ac87f5b3a9c3861ad6e
leonardonm15 <leonardonm15@gmail.com>
2025-10-16 14:03:27 -0300

    kfeita

--- icpc/I_SBC_São_Paulo_Programming_Marathon/k.cpp
+++ icpc/I_SBC_São_Paulo_Programming_Marathon/k.cpp
@@ -5,28 +5,33 @@ using namespace std;
 #define endl '\n' 
 #define int long long
 
-const int N = 4000 + 5;
+const int N = 4e3 + 5;
+const int MOD = 998244353;
 
-int dp[N];
+int dp[N]; // Jump by where i am
 
 void solve () {
     int n, k; cin >> n >> k;
+
     dp[0] = 1;
 
-    for (int i = n; i >= 0; i--) {
-        for (int 
+    for (int jump = n; jump >= 1; jump--) {
+        if (jump == k) continue;
+        for (int i = 0; i <= n; i++) {
+            if (i + jump <= n) {
+                dp[i + jump] += dp[i];
+                dp[i + jump] %= MOD;
+            }
+
+        }
     }
 
+
+    cout << dp[n] << endl;
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
-    int TC = 1;
-    if (TC){
-        cin >> TC;
-        while (TC--) solve();
-    } else {
-        solve();
-    }
+    solve();
     return 0;
 }
